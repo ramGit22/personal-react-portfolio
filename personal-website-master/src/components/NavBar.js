0e483866819f823f112bcddb1fd2bd94d52528ed
Ramesh <karki_ram@hotmail.com>
2023-01-03 20:34:24 +0200

    changes

--- personal-website-master/src/components/NavBar.js
+++ personal-website-master/src/components/NavBar.js
@@ -1,17 +1,14 @@
-import { useState, useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/img/odina2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.png';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import codinglogo from '../assets/img/comprog-logo.png';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export const NavBar = () => {
-
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
@@ -22,20 +19,20 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
-    window.addEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
         <Container>
           <Navbar.Brand href="/">
             <img src={codinglogo} alt="Logo" />
@@ -45,14 +42,37 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('home')}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('skills')}
+              >
+                Skills
+              </Nav.Link>
+              {/* <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link> */}
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/rameshkarr/" target = "_blank"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/ramGit22" target = "_blank"><img src={navIcon2} alt="" /></a>
+                <a
+                  href="https://www.linkedin.com/in/rameshkarr/"
+                  target="_blank"
+                >
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="https://github.com/ramGit22" target="_blank">
+                  <img src={navIcon2} alt="" />
+                </a>
                 {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
               {/* <HashLink to='#connect'>
@@ -63,5 +83,5 @@ export const NavBar = () => {
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
